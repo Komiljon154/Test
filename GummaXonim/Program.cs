fb403a81d518689268cdb31e0faf2c133ad6c064
Karimov Komiljon <komiljonk154@gmail.com>
2025-02-26 11:49:52 +0500

    Update Program.cs

--- GummaXonim/Program.cs
+++ GummaXonim/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
@@ -13,16 +15,31 @@ class Program
 
     static async Task Main(string[] args)
     {
-        var botClient = new TelegramBotClient(BotToken);
+        // Web serverni ishga tushurish
+        var webHost = CreateHostBuilder(args).Build();
 
+        // Telegram botni ishga tushurish
+        var botClient = new TelegramBotClient(BotToken);
         var me = await botClient.GetMeAsync();
         Console.WriteLine($"Bot {me.Username} ishga tushdi!");
 
         botClient.StartReceiving(UpdateHandler, ErrorHandler);
 
+        // Web serverni ishga tushirish
+        var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+        webHost.RunAsync($"http://0.0.0.0:{port}");
+
         Console.WriteLine("Bot ishlayapti. Chiqish uchun Enter tugmasini bosing.");
         Console.ReadLine();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseUrls("http://0.0.0.0:8080") // Portni o'zgartirish mumkin
+                          .UseStartup<Startup>();
+            });
 
     private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
@@ -46,7 +63,7 @@ class Program
                     break;
 
                 case "/obidalar":
-                    var replyMarkup = new InlineKeyboardMarkup(new[]
+                    var replyMarkup = new InlineKeyboardMarkup(new[] 
                     {
                         new[] { InlineKeyboardButton.WithCallbackData("Registon maydoni", "registon") },
                         new[] { InlineKeyboardButton.WithCallbackData("Bibi Xonim masjidi", "bibi_xonim") },
